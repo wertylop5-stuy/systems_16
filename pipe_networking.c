b0c74412ada142243e99377de1b5266ebf3878a5
Stanley Lin <slin55055@gmail.com>
2017-12-12 22:29:18 -0500

    more work

--- pipe_networking.c
+++ pipe_networking.c
@@ -5,23 +5,24 @@
   server_handshake
   args: int * to_client
 
-  Perofrms the client side pipe 3 way handshake.
-  Sets *to_client to the file descriptor to the downstream pipe.
+  Performs the client side pipe 3 way handshake.
+  Sets *to_client to the file descriptor to the downstream pipe. (from server to client)
 
   returns the file descriptor for the upstream pipe.
   =========================*/
 int server_handshake(int *to_client) {
 	//The well known pipe the client will connect to
+	printf("[SERVER] creating well known pipe\n");
 	int wkp = mkfifo(WKP, 0644);
-	printf("[SERVER] created well known pipe");
 	
 	//Whenever client connects, it will read the data that it sends thru
 	char data[HANDSHAKE_BUFFER_SIZE];
+	printf("[SERVER] awaiting connection\n");
 	read(wkp, data, HANDSHAKE_BUFFER_SIZE);
 	
 	//Client has sent name of private FIFO, open it
 	printf("[SERVER] got message from client: %s\n", data);
-	int downstream = open(data, O_WRONLY);
+	*to_client = open(data, O_WRONLY);
 	
 	//Remove the upstream WKP
 	remove(WKP);
@@ -29,15 +30,16 @@ int server_handshake(int *to_client) {
 	
 	//Connect to the private FIFO and send an acknowledgement
 	//strcpy(data, ACK);
-	write(downstream, ACK, strlen(data));
+	printf("[SERVER] sending acknowledgement to client\n");
+	write(*to_client, ACK, strlen(data));
 	
 	//Now, wait for client to send back the acknowledgement
-	read(downstream, data, sizeof(data));
+	read(wkp, data, sizeof(data));
 	
-	//Check 
-	if (
-	
-	//Communicate
+	//Check if ACK and data match
+	if (!strcmp(data, ACK)) {
+		printf("[SERVER] received acknowledgement back\n");
+	}
 	
 	return wkp;
 }
@@ -47,7 +49,7 @@ int server_handshake(int *to_client) {
   client_handshake
   args: int * to_server
 
-  Perofrms the client side pipe 3 way handshake.
+  Performs the client side pipe 3 way handshake.
   Sets *to_server to the file descriptor for the upstream pipe.
 
   returns the file descriptor for the downstream pipe.
